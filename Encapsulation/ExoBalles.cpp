792be7b73f5cb41afea82b2e6f68f8ff95116754
Lynn <jacquelinehuynh@outlook.fr>
2024-10-10 22:48:01 +0200

    Balle qui bouge

--- Encapsulation/ExoBalles.cpp
+++ Encapsulation/ExoBalles.cpp
@@ -1,82 +1,109 @@
 #include <SDL.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class Balles{
+class Balles {
 private:
-	int x;
-	int y;
-	int posx;
-	int posy;
+    int x;      
+    int y;      
+    int rayon;  
+    int vitesse;
 public:
-	void bal(int x1, int y1, int pos1, int pos2) {
-		x = x1;
-		y = y1;
-		posx = pos1;
-		posy = pos2;
-	}
-	void ball(SDL_Renderer* renderer) {
-		SDL_Rect r;
-		r.x = posx;
-		r.y = posy;
-		r.w = x;
-		r.h = y;
+    Balles (int x1, int y1, int r) {
+        x = x1;      
+        y = y1;      
+        rayon = r;   
+        vitesse = 4;
+    }
 
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderFillRect(renderer, &r);
-	}
+    // Fonction pour dessiner un cercle
+    void drawCircle(SDL_Renderer* renderer) {
+        
+        for (int w = 0; w < rayon * 2; w++) {
+            for (int h = 0; h < rayon * 2; h++) {
+                int dx = rayon - w; // Distance horizontale par rapport au centre
+                int dy = rayon - h; // Distance verticale par rapport au centre
+                if ((dx * dx + dy * dy) <= (rayon * rayon)) {
+                    SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+                }
+            }
+        }
+    }
+    void moveCircle(SDL_Renderer* renderer) {
+        x += vitesse;
+        y += vitesse;
+
+        if (x + rayon > 960) {
+            x -= vitesse;
+        }
+        if (y + rayon > 540) {
+            y -= vitesse;
+        }
+    }
 };
 
 int main(int argc, char* args[]) {
-	
-	SDL_Window* window = NULL;
-	SDL_Renderer* renderer = NULL;
-	
-	// Initialise SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		cout << "Error initializing SDL: " << SDL_GetError() << endl;
-		system("pause");
-	}
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
 
-	// Creation de la fenêtre
-	window = SDL_CreateWindow("Rebond de balles", SDL_WINDOWPOS_UNDEFINED, 
-		SDL_WINDOWPOS_UNDEFINED, 960, 540, SDL_WINDOW_SHOWN);
+    // Initialise SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "Error initializing SDL: " << SDL_GetError() << endl;
+        return -1;
+    }
 
-	//Message en cas d'erreur de création
-	if (!window) {
-		cout << "Error creating window: " << SDL_GetError() << endl;
-		system("pause");
-	}
+    // Création de la fenêtre
+    window = SDL_CreateWindow("Rebond de balles", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, 960, 540, SDL_WINDOW_SHOWN);
 
-	// Obtenir la surface de la fenêtre
-	renderer = SDL_CreateRenderer(window, 0,
-		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // Message en cas d'erreur de création
+    if (!window) {
+        cout << "Error creating window: " << SDL_GetError() << endl;
+        SDL_Quit();
+        return -1;
+    }
 
-	//Message en cas d'erreur d'obtention
-	if (!renderer) {
-		cout << "Error getting surface: " << SDL_GetError() << endl;
-		system("pause");
-	}
+    // Obtenir le renderer de la fenêtre
+    renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	Balles b;	
-	b.bal(50, 50, 50, 80);
+    // Message en cas d'erreur d'obtention
+    if (!renderer) {
+        cout << "Error getting renderer: " << SDL_GetError() << endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-	//Carre
-	while (1)
-	{
-		SDL_SetRenderDrawColor(renderer, 63, 63, 105, 255);
-		SDL_RenderClear(renderer);
-		b.ball(renderer);
-		SDL_RenderPresent(renderer);
-		
-	}
+    Balles b (100, 250, 25);
 
-	//Détruit tout avant de quitter la fenêtre
-	SDL_DestroyWindow(window);
-	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
+    bool quit = false;
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
 
-	return 0;
+        // Nettoyage de l'écran
+        SDL_SetRenderDrawColor(renderer, 63, 63, 105, 255);
+        SDL_RenderClear(renderer);
+
+        // Dessiner le cercle et faire bouger
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Couleur du cercle
+        b.drawCircle(renderer);
+        b.moveCircle(renderer);
+
+        // Afficher à l'écran
+        SDL_RenderPresent(renderer);
+    }
+
+    // Détruit tout avant de quitter
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
+    return 0;
 }
