a51f5299cea31cf07348c809f5877a81f1de9c1c
Lynn <jacquelinehuynh@outlook.fr>
2024-10-14 17:52:08 +0200

    creation projet

--- Encapsulation/projet.cpp
+++ Encapsulation/projet.cpp
@@ -2,27 +2,34 @@
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
+#include<list>
+#include<time.h>
 using namespace std;
 
-/*class Balles {
+int fps = 60;
+int desiredDelta = 1000 / fps;
+int frameCount = 0;
+int fpsTimer = SDL_GetTicks();
+
+class Balles {
 private:
-    int x;      
-    int y;      
-    int rayon;  
+    int x;
+    int y;
+    int rayon;
     int vitessex;
     int vitessey;
 public:
-    Balles (int x1, int y1, int r) {
-        x = x1;      
-        y = y1;      
-        rayon = r;   
+    Balles(int x1, int y1, int r) {
+        x = x1;
+        y = y1;
+        rayon = r;
         vitessex = 4;
         vitessey = 4;
     }
 
     // Fonction pour dessiner un cercle
     void drawCircle(SDL_Renderer* renderer) {
-        
+
         for (int w = 0; w < rayon * 2; w++) {
             for (int h = 0; h < rayon * 2; h++) {
                 int dx = rayon - w; // Distance horizontale par rapport au centre
@@ -37,10 +44,10 @@ public:
         x += vitessex;
         y += vitessey;
 
-        if ((x - rayon < 0)||(x + rayon > 960)) {
+        if ((x - rayon < 0) || (x + rayon > 960)) {
             vitessex = -vitessex;
         }
-        if ((y - rayon < 0) ||(y + rayon > 540)) {
+        if ((y - rayon < 0) || (y + rayon > 540)) {
             vitessey = -vitessey;
         }
     }
@@ -60,11 +67,13 @@ public:
             }
         }
     }
+
 };
 
 int main(int argc, char* args[]) {
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
+    srand(time(NULL));
 
     // Initialise SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -94,9 +103,8 @@ int main(int argc, char* args[]) {
         return -1;
     }
 
-    Balles b (100, 250, 25);
-    Balles c (850, 50, 25);
-    
+    list<Balles> ballesList;
+    Uint32 lastBallAddedTime = SDL_GetTicks();  // Timer to track time since last ball was added
 
     bool quit = false;
     SDL_Event e;
@@ -107,23 +115,53 @@ int main(int argc, char* args[]) {
             }
         }
 
+        int starttick = SDL_GetTicks();
+
         // Nettoyage de l'écran
         SDL_SetRenderDrawColor(renderer, 63, 63, 105, 255);
         SDL_RenderClear(renderer);
 
-        // Dessiner le cercle et faire bouger
-        SDL_SetRenderDrawColor(renderer, 143, 219, 177, 255); // Couleur du cercle
-        b.drawCircle(renderer);
-        b.moveCircle(renderer);
-        //Cercle 2
-        SDL_SetRenderDrawColor(renderer, 143, 192, 219, 255);
-        c.drawCircle(renderer);
-        c.moveCircle(renderer);
-        b.collisionBalls(c);
+        // Ajouter une nouvelle balle toutes les 2 secondes (2000 ms)
+        if (SDL_GetTicks() - lastBallAddedTime > 2000) {
+            if (ballesList.size() < 100) {
+                ballesList.push_back(Balles(rand() % 950, rand() % 530, 25));
+            }
+            lastBallAddedTime = SDL_GetTicks();
+        }
+
+        // Dessiner et déplacer chaque balle
+        for (auto it = ballesList.begin(); it != ballesList.end(); ++it) {
+            SDL_SetRenderDrawColor(renderer, 143, 219, 177, 255);
+            it->drawCircle(renderer);
+            it->moveCircle(renderer);
+        }
 
         // Afficher à l'écran
         SDL_RenderPresent(renderer);
+
+        int delta = SDL_GetTicks() - starttick; // Temps écoulé depuis le début de la frame
+
+        // Calcul du temps restant pour atteindre 60 FPS
+        int remainingTime = desiredDelta - delta;
+        if (remainingTime > 0) {
+            SDL_Delay(remainingTime);
+        }
+        else {
+            SDL_Delay(1);
+        }
+
+        // Compter les frames
+        frameCount++;
+
+        // Affichage des FPS
+        if (SDL_GetTicks() - fpsTimer >= 1000) {
+            std::cout << "FPS: " << frameCount << std::endl;
+            frameCount = 0;  // Réinitialiser le compteur de frames
+            fpsTimer += 1000; // Réinitialiser le timer
+        }
+        
     }
+
 
     // Détruit tout avant de quitter
     SDL_DestroyWindow(window);
@@ -131,4 +169,4 @@ int main(int argc, char* args[]) {
     SDL_Quit();
 
     return 0;
-}*/
+}
